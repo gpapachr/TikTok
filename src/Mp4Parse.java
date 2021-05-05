9a5c9479c79352ac1e7bd68280d30fb015b4d2cd
Fotios Panos <80756565+fotispanos@users.noreply.github.com>
2021-05-05 20:17:04 +0300

    videooooooooooos

--- src/Mp4Parse.java
+++ src/Mp4Parse.java
@@ -11,6 +11,118 @@ import org.apache.tika.sax.BodyContentHandler;
 
 import org.xml.sax.SAXException;
 
+import java.io.*;
+
+
+public class Mp4Parse {
+    
+            File file = new File("C:/Documents/Despicable Me 2 - Trailer (HD) - YouTube.mp4");//File read from Source folder to Split.
+            if (file.exists()) {
+
+            String videoFileName = file.getName().substring(0, file.getName().lastIndexOf(".")); // Name of the videoFile without extension
+            File splitFile = new File("C:/Documents/Videos_Split/"+ videoFileName);//Destination folder to save.
+            if (!splitFile.exists()) {
+                splitFile.mkdirs();
+                System.out.println("Directory Created -> "+ splitFile.getAbsolutePath());
+            }
+
+            int i = 01;// Files count starts from 1
+            InputStream inputStream = new FileInputStream(file);
+            String videoFile = splitFile.getAbsolutePath() +"/"+ String.format("%02d", i) +"_"+ file.getName();// Location to save the files which are Split from the original file.
+            OutputStream outputStream = new FileOutputStream(videoFile);
+            System.out.println("File Created Location: "+ videoFile);
+            int chunkSize = 512 * 1024;
+            int totalPartsToSplit = inputStream.available() / chunkSize;
+            int streamSize = 0;
+            int read = 0;
+            while ((read = inputStream.read()) != -1) {
+
+                if (splitSize == streamSize) {
+                    if (i != totalPartsToSplit) {
+                        i++;
+                        String fileCount = String.format("%02d", i); // output will be 1 is 01, 2 is 02
+                        videoFile = splitFile.getAbsolutePath() +"/"+ fileCount +"_"+ file.getName();
+                        outputStream = new FileOutputStream(videoFile);
+                        System.out.println("File Created Location: "+ videoFile);
+                        streamSize = 0;
+                    }
+                }
+                outputStream.write(read);
+                streamSize++;
+            }
+
+            inputStream.close();
+            outputStream.close();
+            System.out.println("Total files Split ->"+ totalPartsToSplit);
+        } else {
+            System.err.println(file.getAbsolutePath() +" File Not Found.");
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*private class ChunkHandler {
+   private int size = 0;
+   private int fileNumber = -1;
+   private OutputStreamWriter out = null;
+
+   final byte maxChunkSize = 512 * 1024;
+   final File outputDir = new File("/tmp/");
+
+   public void bytes(byte[] b, int start, int length) throws IOException {
+      if (out == null || size+length > maxChunkSize) {
+         if (out != null) out.close();
+         fileNumber++;
+         File f = new File(outputDir, "output-" + fileNumber + ".mp4");
+         out = new OutputStreamWriter(new FileOutputStream(f, "UTF-8"));
+      }
+      out.write(b, start, length);
+   }
+   public void close() throws IOException {
+      if (out != null) out.close();
+   }
+}
+
+public void parse(File file) {
+   InputStream stream = new FileInputStream(file);
+   Parser p = new AutoDetectParser();
+   Metadata meta =new Metadata();
+   ContentHandler handler = new ChunkHandler();
+   ParseContext parse = new ParseContext();
+
+   p.parse(stream,handler,meta, context);
+   ((ChunkHandler)handler).close();
+}
+
+
 public class Mp4Parse {
 
    public static void main(final String[] args) throws IOException,SAXException, TikaException {
@@ -32,4 +144,4 @@ public class Mp4Parse {
          System.out.println(name + ": " + metadata.get(name));
       }
    }  
-}
+}*/
--- src/VideoFile.java
+++ src/VideoFile.java
@@ -1,39 +1,93 @@
 import java.util.*;
+import java.io.File;
+import org.mp4parser.IsoFile;
+import org.mp4parser.boxes.iso14496.part12.MovieHeaderBox;
 
 public class VideoFile {
     private String videoName;
     private String channelName;
     private String dateCreated;
-    private String length;
+    private long length;
     private String framerate;
     private String frameWidth;
     private String frameHeight;
-    private ArrayList<String> associatedHashtags;
-    private byte videoFileChunk;
+    private ArrayList<String> associatedHashtags = new ArrayList<String>();
+    private byte[] videoFileChunk;
+    private String path;
 
-    public String getVideoName() {
-        return videoName;
-    }
 
-    public void setVideoName(String videoName) {
-        this.videoName = videoName;
-    }
-
-    public String getChannelName() {
-        return channelName;
+    VideoFile(String s) {
+        this.path = s;
+        File video = new File(path);
+        this.length = getVideoLength(video);
     }
 
     public void setChannelName(String channelName) {
         this.channelName = channelName;
     }
 
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public void setAssociatedHashtags(ArrayList<String> ah) {
+        for (int i = 0; i < ah.size(); i++) {
+            associatedHashtags.add(ah.get(i));
+        }
+    }
+
+    public void setVideoName(String videoName) {
+        this.videoName = videoName;
+    }
+
+    public byte[] getVideoFileChunk() {
+        return null;
+    }
+
+    public void setVideoFileChunk(byte videoFileChunk) {
+        this.videoFileChunk = videoFileChunk;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String getVideoName() {
+        return videoName;
+    }
+
+    
+
+    
+
+   
+
     public String getDateCreated() {
         return dateCreated;
     }
 
-    public void setDateCreated(String dateCreated) {
-        this.dateCreated = dateCreated;
-    }
+    
 
     public String getLength() {
         return length;
@@ -71,15 +125,7 @@ public class VideoFile {
         return associatedHashtags;
     }
 
-    public void setAssociatedHashtags(ArrayList<String> associatedHashtags) {
-        this.associatedHashtags = associatedHashtags;
-    }
+   
 
-    public byte getVideoFileChunk() {
-        return videoFileChunk;
-    }
-
-    public void setVideoFileChunk(byte videoFileChunk) {
-        this.videoFileChunk = videoFileChunk;
-    }
+    
 }
