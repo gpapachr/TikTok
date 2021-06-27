2ac2bc13ba77ada0529f9ea6dc19a9a1ea23c901
gpapachr <giannis.papachristou@yahoo.gr>
2021-06-27 21:44:01 +0300

    camera eists but doesnt work

--- TikTokApp/app/src/main/java/com/example/myapplication/TikTokApp/PublisherMainActivity.java
+++ TikTokApp/app/src/main/java/com/example/myapplication/TikTokApp/PublisherMainActivity.java
@@ -1,22 +1,36 @@
 package com.example.myapplication.TikTokApp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.*;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.*;
 
 public class PublisherMainActivity extends AppCompatActivity {
 
+    private static int VIDEO_REQUEST = 101;
+    private Uri videoUri = null;
+
     Button cancel;
     Button upload;
+    Button capture;
+
     EditText file;
     EditText name;
     EditText hashtag;
+
     String username;
+
+    VideoView videoView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +39,13 @@ public class PublisherMainActivity extends AppCompatActivity {
 
         cancel = (Button) findViewById(R.id.publish_abort_btn);
         upload = (Button) findViewById(R.id.upload_btn);
+        capture = (Button) findViewById(R.id.capture_video_btn);
+
         file = (EditText) findViewById(R.id.editTextVideoPath);
         name = (EditText) findViewById(R.id.editTextVideoName);
         hashtag = (EditText) findViewById(R.id.editTextHashtags);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
 
         Intent intent = getIntent();
         username = intent.getStringExtra("username");
@@ -64,6 +82,27 @@ public class PublisherMainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(intent, VIDEO_REQUEST);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == VIDEO_REQUEST){
+            videoUri = data.getData();
+            videoView.setVideoURI(videoUri);
+            videoView.start();
+        }
     }
 
     private class PublisherRunner extends AsyncTask<String, String, String> {
